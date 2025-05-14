0d005320e7e7d4493aabb5ee499d964edae3f919
phucdh410 <phucdh410@gmail.com>
2025-05-15 01:07:00 +0700

    UPDATE: code

--- More-Drops/scripts/beebox.lua
+++ More-Drops/scripts/beebox.lua
@@ -2,11 +2,15 @@ local beeboxMultiple = GetModConfigData("beeboxMultiple", KnownModIndex:GetModAc
 
 local function ImproveBeebox(inst)
   if inst.components.harvestable then
-			local old_onharvest = inst.components.harvestable.onharvestfn
-			inst.components.harvestable.onharvest = function(self, picker, produce)
-					local new_produce = math.floor(produce * beeboxMultiple)
-					old_onharvest(self, picker, new_produce)
-			end
+		print("MULTIPLE",beeboxMultiple)
+		local old_onharvest = inst.components.harvestable.onharvestfn
+		print("OLDHARVEST",old_onharvest)
+		inst.components.harvestable:SetOnHarvestFn(function(self, picker, produce, ...)
+			print("ORG_PRODUCE",produce)
+			local new_produce = math.floor(produce * beeboxMultiple)
+			print("NEW_PRODUCE",new_produce)
+			return old_onharvest(self, picker, new_produce, ...)
+		end)
 	end
 end
 
