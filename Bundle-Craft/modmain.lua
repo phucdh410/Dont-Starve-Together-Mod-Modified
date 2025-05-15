b340e1b9d938bdda2fec41e7c62810a665834ce2
phucdh410 <phucdh410@gmail.com>
2025-05-15 14:22:40 +0700

    UPDATE: new mod

---
+++ Bundle-Craft/modmain.lua
@@ -0,0 +1,3 @@
+AddRecipe2("bundle_cutstones", {Ingredient("rocks", 150)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/cutstone.xml", image = "cutstone.tex", product = "cutstone", numtogive = 50 }, { "REFINE" })
+AddRecipe2("bundle_ropes", {Ingredient("cutgrass", 150)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/rope.xml", image = "rope.tex", product = "rope", numtogive = 50 }, { "REFINE" })
+AddRecipe2("bundle_boards", {Ingredient("log", 200)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/boards.xml", image = "boards.tex", product = "boards", numtogive = 50 }, { "REFINE" })
