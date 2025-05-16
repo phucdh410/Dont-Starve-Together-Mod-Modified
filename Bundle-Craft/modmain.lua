6479497a5086497745148ae37776f0916e9bceed
phucdh410 <phucdh410@gmail.com>
2025-05-16 17:29:20 +0700

    Commit

--- Bundle-Craft/modmain.lua
+++ Bundle-Craft/modmain.lua
@@ -1,3 +1,19 @@
-AddRecipe2("bundle_cutstones", {Ingredient("rocks", 150)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/cutstone.xml", image = "cutstone.tex", product = "cutstone", numtogive = 50 }, { "REFINE" })
-AddRecipe2("bundle_ropes", {Ingredient("cutgrass", 150)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/rope.xml", image = "rope.tex", product = "rope", numtogive = 50 }, { "REFINE" })
-AddRecipe2("bundle_boards", {Ingredient("log", 200)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/boards.xml", image = "boards.tex", product = "boards", numtogive = 50 }, { "REFINE" })
+local STRINGS = GLOBAL.STRINGS
+
+AddRecipe2("bundle_cutstones",
+  {Ingredient("rocks", 150)},
+    TECH.SCIENCE_ONE,
+  {
+    atlas = "images/inventoryimages/bundle_cutstones.xml",
+    image = "bundle_cutstones.tex",
+    product = "cutstone",
+    numtogive = 50 
+  },
+  { "REFINE" }
+)
+
+STRINGS.NAMES.BUNDLE_CUTSTONES = "50x Cutstones"
+STRING.RECIPE_DESC.BUNDLE_CUTSTONES = "Craft instantly 50 cutstones"
+
+-- AddRecipe2("bundle_ropes", {Ingredient("cutgrass", 150)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/rope.xml", image = "rope.tex", product = "rope", numtogive = 50 }, { "REFINE" })
+-- AddRecipe2("bundle_boards", {Ingredient("log", 200)}, TECH.SCIENCE_ONE, {atlas = "images/inventoryimages/boards.xml", image = "boards.tex", product = "boards", numtogive = 50 }, { "REFINE" })
