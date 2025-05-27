2ca5d7e1d45d74558488ac009844bce9244c5c0d
phucdh410 <phucdh410@gmail.com>
2025-05-27 15:15:52 +0700

    Add new mod

---
+++ Gem-Crystal-Cluster/modinfo.lua
@@ -0,0 +1,46 @@
+name = "Gem Crystal Cluster"
+description = ""
+author = ""
+version = "1.2"
+
+forumthread = ""
+
+dst_compatible = true
+all_clients_require_mod = true
+api_version = 10
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+configuration_options =
+{
+    {
+        name = "growtime",
+        label = "Growth Cycle (day)",
+        hover = "Growth Cycle (day)",
+        options =
+        {
+            {description = "1", data = 1},
+            {description = "3", data = 3},
+            {description = "5", data = 5},
+            {description = "8", data = 8},
+            {description = "10", data = 10},
+        },
+        default = 5,
+    },
+    {
+        name = "gemcost",
+        label = "Gem Cost",
+        hover = "Gem Cost",
+        options =
+        {
+            {description = "1", data = 1},
+            {description = "2", data = 2},
+            {description = "3", data = 3},
+            {description = "5", data = 5},
+            {description = "8", data = 8},
+            {description = "10", data = 10},
+        },
+        default = 5,
+    },
+}
