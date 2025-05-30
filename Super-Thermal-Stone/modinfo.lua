335fb923a8a11efbaf6bdcd60a7466e7a4882fdc
phucdh410 <phucdh410@gmail.com>
2025-05-30 10:52:25 +0700

    Add new mod

---
+++ Super-Thermal-Stone/modinfo.lua
@@ -0,0 +1,17 @@
+name = "Imperishable Thermal Stone"
+description = "Prevents durability loss in thermal stones like it was originally in Don't Starve."
+author = "Morback"
+version = "1.0"
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "imperishable_thermal_stone.xml"
+icon = "imperishable_thermal_stone.tex"
+
+-- Specify compatibility with the game!
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = false
---
+++ Super-Thermal-Stone/modmain.lua
@@ -0,0 +1,95 @@
+-- #############################################################################
+-- Removes Thermal Stone durability
+-- #############################################################################
+
+-- These represent the boundaries between the ranges (relative to ambient, so ambient is always "0")
+relative_temperature_thresholds = { -30, -10, 10, 30 }
+
+function GetRangeForTemperature(temp, ambient)
+    local range = 1
+    for i,v in ipairs(relative_temperature_thresholds) do
+        if temp > ambient + v then
+            range = range + 1
+        end
+    end
+    return range
+end
+
+function UpdateImages(inst, range)
+    inst.currentTempRange = range
+
+    inst.AnimState:PlayAnimation(tostring(range), true)
+
+    local skinname = inst:GetSkinName()
+    inst.components.inventoryitem:ChangeImageName((skinname or "heat_rock")..tostring(range))
+    if range == 5 then
+        inst.AnimState:SetBloomEffectHandle("shaders/anim.ksh")
+        inst._light.Light:Enable(true)
+    else
+        inst.AnimState:ClearBloomEffectHandle()
+        inst._light.Light:Enable(false)
+    end
+end
+
+function AdjustLighting(inst, range, ambient)
+    if range == 5 then
+        local relativetemp = inst.components.temperature:GetCurrent() - ambient
+        local baseline = relativetemp - relative_temperature_thresholds[4]
+        local brightline = relative_temperature_thresholds[4] + 20
+        inst._light.Light:SetIntensity( math.clamp(0.5 * baseline/brightline, 0, 0.5 ) )
+    else
+        inst._light.Light:SetIntensity(0)
+    end
+end
+
+AddPrefabPostInit("heatrock", function (inst)
+    if not GLOBAL.TheWorld.ismastersim then
+        return
+    end
+
+    -- Removes the old temperaturedelta EventListener function
+    inst.event_listeners["temperaturedelta"] = {}
+    -- Alter the TemperatureChange(inst, data) function
+    inst:ListenForEvent("temperaturedelta", function (inst, data)
+        local ambient_temp = GLOBAL.TheWorld.state.temperature
+        local cur_temp = inst.components.temperature:GetCurrent()
+        local range = GetRangeForTemperature(cur_temp, ambient_temp)
+
+        AdjustLighting(inst, range, ambient_temp)
+
+        if range <= 1 then
+            if inst.lowTemp == nil or inst.lowTemp > cur_temp then
+                inst.lowTemp = math.floor(cur_temp)
+            end
+            inst.highTemp = nil
+        elseif range >= 5 then
+            if inst.highTemp == nil or inst.highTemp < cur_temp then
+                inst.highTemp = math.ceil(cur_temp)
+            end
+            inst.lowTemp = nil
+        elseif inst.lowTemp ~= nil then
+            if GetRangeForTemperature(inst.lowTemp, ambient_temp) >= 3 then
+                inst.lowTemp = nil
+            end
+        elseif inst.highTemp ~= nil and GetRangeForTemperature(inst.highTemp, ambient_temp) <= 3 then
+            inst.highTemp = nil
+        end
+
+        if range ~= inst.currentTempRange then
+            UpdateImages(inst, range)
+
+            if (inst.lowTemp ~= nil and range >= 3) or
+                (inst.highTemp ~= nil and range <= 3) then
+                inst.lowTemp = nil
+                inst.highTemp = nil
+                --inst.components.fueled:SetPercent(inst.components.fueled:GetPercent() - 1 / TUNING.HEATROCK_NUMUSES)
+            end
+        end
+    end)
+
+    inst.currentTempRange = 0
+
+    if inst and inst.components and inst.components.fueled then
+        inst:RemoveComponent("fueled")
+    end
+end)
