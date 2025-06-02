536e110c19f66321359e0b3b861bda35385feb73
phucdh410 <phucdh410@gmail.com>
2025-06-02 16:29:18 +0700

    UPDATE: add test mods

---
+++ Test-Poison/modinfo.lua
@@ -0,0 +1,8 @@
+name = "TEST - Poison"
+description = "Just a test mod"
+author = "PhucDH410"
+version = "1.0"
+forumthread = ""
+dst_compatible = true
+all_clients_require_mod = true
+api_version = 10
---
+++ Test-Skilltree/modinfo.lua
@@ -0,0 +1,15 @@
+name = "TEST - Insight"
+description = "Test mod"
+author = "PhucDH410"
+
+version = "1.0"
+forumthread = ""
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+all_clients_require_mod = false
+client_only_mod = true
+
+configuration_options = { }
