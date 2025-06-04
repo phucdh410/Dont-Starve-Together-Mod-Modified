8715992441aa1bf116d774916aa7af9b23c21fd7
phucdh410 <phucdh410@gmail.com>
2025-06-04 14:11:17 +0700

    Remove Chinese localization files and add English localization for UI elements in Efficient Agriculture mod. Update product list formatting for consistency.

--- Efficient-Agriculture/modmain.lua
+++ Efficient-Agriculture/modmain.lua
@@ -46,43 +46,12 @@ Assets = {
     Asset("IMAGE", "images/ea_filter.tex"),
 }
 
+modimport("scripts/common.lua")
 
--------------------------
--- [文本本地化]
--------------------------
-local mod_loc_files = {         --本mod的所有本地化文件名
-    "common",                   --无法归类或没必要归类的文本
-    "ui",                       --界面相关文本
-}
+modimport("scripts/ui.lua")
 
-local user_setting_lan = GetModConfigData("language")       --读取Mod语言自定义设置
-local loc = require "languages/loc"
-local lan = loc and loc.GetLanguage and loc.GetLanguage()   --读取玩家的客户端语言设置
-local folder = ""
-if user_setting_lan == "CHINESE" then
-    folder = "zh-cn"
-elseif user_setting_lan == "ENGLISH" then
-    folder = "en"
-else
-    if lan == LANGUAGE.CHINESE_S or lan == LANGUAGE.CHINESE_S_RAIL then
-        folder = "zh-cn"
-    else
-        folder = "en"
-    end
-end
-assert(folder and folder ~= "", "[Efficient Agriculture Error] Failed to load loc file")    --语言设置报错
-for _, f_name in ipairs(mod_loc_files) do
-    modimport("scripts/localization/"..folder.."/"..f_name)     --加载所有本地化文件
-end
-
-
---------------------------
--- main_scripts
---------------------------
--- [全局常量(大量mod数据配置)]
 modimport("ea_main_scripts/constants.lua")
 
--- [加入机器prefab]
 if EA_CONSTANTS.ADDED_HARVEST then
     table.insert(PrefabFiles,"ea_harvest_machine")
 end
@@ -93,35 +62,22 @@ if EA_CONSTANTS.ADDED_SHOP then
     table.insert(PrefabFiles,"ea_shop")
 end
 
--- [工具函数]
 modimport("ea_main_scripts/tools.lua")
 
--- [注册RPC]
 modimport("ea_main_scripts/rpc.lua")
 
---[Controller]
 modimport("ea_main_scripts/controller.lua")
 
---[Containers]
 modimport("ea_main_scripts/containers.lua")
 
--- [UI修改]
 modimport("ea_main_scripts/ui.lua")
 
--- [SG]
--- modimport("ea_main_scripts/sg.lua")
-
--- [Actions]
 modimport("ea_main_scripts/actions.lua")
 
--- [配方]
 modimport("ea_main_scripts/recipes.lua")
 
--- [Replica Components]
 AddReplicableComponent("ea_rectangle")
 
-
--- [小地图图标注册]
 AddMinimapAtlas("images/minimap/ea_fertilizer_machine_mini.xml")
 AddMinimapAtlas("images/minimap/ea_seeding_machine_mini.xml")
 AddMinimapAtlas("images/minimap/ea_harvest_machine_mini.xml")
@@ -130,11 +86,6 @@ AddMinimapAtlas("images/minimap/ea_farm_plow_machine_mini.xml")
 AddMinimapAtlas("images/minimap/ea_shop_mini.xml")
 AddMinimapAtlas("images/minimap/ea_well_mini.xml")
 
--------------------------
--- [针对本MOD的其它修改]
--------------------------
-
--- 金坷垃可研究
 local FERTILIZER_DEFS = require("prefabs/fertilizer_nutrient_defs").FERTILIZER_DEFS
 local SORTED_FERTILIZERS = require("prefabs/fertilizer_nutrient_defs").SORTED_FERTILIZERS
 
@@ -147,7 +98,6 @@ FERTILIZER_DEFS.ea_jinkela = {
     uses = 1,
 }
 
--- 显示旋转收获机提示文本
 local rotable_placers = {}
 function _G.RegisterRotablePlacer(prefab_name)
     if type(prefab_name) == "string" then
@@ -166,8 +116,6 @@ AddComponentPostInit("playercontroller", function(self)
     end
 end)
 
--- [Camera]
--- 视角锁定(通过设置摄像机的lock_heading_target属性为true来锁定视角)
 AddGlobalClassPostConstruct("cameras/followcamera", "FollowCamera", function(self)
     local old_SetHeadingTarget = self.SetHeadingTarget
     self.SetHeadingTarget = function(_self, r)
